169a03c99946edc664eec31c9cd34a65bd2eb4ba
riko2-afk <112545785+riko2-afk@users.noreply.github.com>
2023-01-20 15:43:19 +0700

    Add files via upload

--- riko.py
+++ riko.py
@@ -1,3 +1,8 @@
 number1 = 70
 number2 = 80
 ans = input("do you want to add")
+if answer == !"Y":
+    result = number1 + number2
+    else:
+        result = number1 - number2 
+        print(result)
